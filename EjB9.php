25bf4b5ad23834afa33d751420bc98535005631c
Manuela Cardona Arias <manuelita9688@gmail.com>
2025-10-27 13:55:46 +0100

    Ejercicio bloque 9 terminado

---
+++ EjB9.php
@@ -0,0 +1,8 @@
+<?php
+		
+		$headache = true;
+		require_once 'headache_advice.php';
+        /*Escogi el require_once porque sin el archio externo
+        el index no funcionaria y utilice el requiere_once para que no 
+        se de duplicidad en los archivos. */
+?>
---
+++ headache_advice.php
@@ -0,0 +1,9 @@
+<?php
+    if ($headache== "true") {
+        // Si es verdadaero muestra esto:
+        echo "Te duele la cabeza, te recomendamos que tomes un ibuprofeno para aliviar el dolor.";
+    } else {
+        // Si no es verdadero muestra esto:
+        echo  "No te duele la cabeza. ¡Todo está bien!";
+    }
+?>
